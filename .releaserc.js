926cfed5a3c50e73c6edc623b0fae108b2c1c7e4
nikaera <gimnikaerast@gmail.com>
2021-06-20 12:19:03 +0900

    :rotating_light: fix: grant execution privileges to husky

--- .releaserc.js
+++ .releaserc.js
@@ -1,37 +1,37 @@
 module.exports = {
-branches: ["main"],
-plugins: [
+  branches: ["main"],
+  plugins: [
     [
-    "@semantic-release/commit-analyzer",
-    {
+      "@semantic-release/commit-analyzer",
+      {
         config: "conventional-changelog-gitmoji-config",
         releaseRules: [{ type: "build", release: "patch" }],
-    },
+      },
     ],
     [
-    "@semantic-release/release-notes-generator",
-    {
+      "@semantic-release/release-notes-generator",
+      {
         config: "conventional-changelog-gitmoji-config",
-    },
+      },
     ],
     [
-    "@semantic-release/github",
-    {
+      "@semantic-release/github",
+      {
         assets: [
-        {
+          {
             path: "react-emoji-textarea.zip",
             label: "Teemo - Chrome Extension",
-        },
+          },
         ],
-    },
+      },
     ],
     [
-    "@semantic-release/git",
-    {
+      "@semantic-release/git",
+      {
         message:
-        // eslint-disable-next-line no-template-curly-in-string
-        ":bookmark: chore(release): ${nextRelease.gitTag} [skip ci] \n\n${nextRelease.notes}",
-    },
+          // eslint-disable-next-line no-template-curly-in-string
+          ":bookmark: chore(release): ${nextRelease.gitTag} [skip ci] \n\n${nextRelease.notes}",
+      },
     ],
-],
+  ],
 };

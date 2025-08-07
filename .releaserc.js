2aa5e5a5559382fc1f03db0ba07c652655499d16
nikaera <gimnikaerast@gmail.com>
2025-08-07 17:29:33 +0900

    :bug: fix: Refactor code structure for improved readability and maintainability

--- .releaserc.js
+++ .releaserc.js
@@ -11,6 +11,13 @@ module.exports = {
       '@semantic-release/release-notes-generator'
     ],
     [
+      '@semantic-release/npm',
+      {
+        npmPublish: true,
+        pkgRoot: 'dist',
+      },
+    ],
+    [
       '@semantic-release/github',
       {
         assets: [
--- dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-export * from './EmojiTextArea';
-export { default as EmojiTextArea } from './EmojiTextArea';

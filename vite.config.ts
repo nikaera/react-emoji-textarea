5ed8047b208104c20bb4504b388a7d01f2efccb4
nikaera <gimnikaerast@gmail.com>
2025-08-07 16:17:32 +0900

    :art: refactor: rename ReactEmojiTextarea to ReactEmojiTextArea in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: 'src/index.ts',
-      name: 'ReactEmojiTextarea',
+      name: 'ReactEmojiTextArea',
       fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
